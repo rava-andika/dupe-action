7f37ab873c0d48703ab746ca2d33902198c1550d
Rava Dwi Andika <gendonsumbersari@gmail.com>
2026-07-08 22:50:50 +0700

    Add files via upload

---
+++ database/migrations/2025_07_07_113525_create_user_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->date('birth_date');
+            $table->string('phone_number');
+            $table->string('province');
+            $table->string('address');
+            $table->string('institution');
+            $table->string('student_id');
+            $table->string('institution_card')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
